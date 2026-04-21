74e36c582c47449126b26f4041b56a6bbcd5d8d7
luzhoua <luzhoua@MacBook-Pro.local>
2026-04-21 13:21:53 +0800

    refs MIN-164 fix: reduce page top padding to match Product section spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <Navbar />
-          <div className="relative z-10 mx-auto max-w-6xl px-5 pb-20 pt-[4.8rem] sm:px-6 sm:pt-[5.2rem]">
+          <div className="relative z-10 mx-auto max-w-6xl px-5 pb-20 pt-[3.2rem] sm:px-6 sm:pt-[3.6rem]">
             {children}
           </div>
         </ThemeProvider>
